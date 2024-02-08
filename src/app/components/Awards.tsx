aec385045d9b1ff87570a120f3d8dee3f594c71e
dham19 <david.ham0099@gmail.com>
2024-02-08 00:07:12 -0500

    formatting

--- src/app/components/Awards.tsx
+++ src/app/components/Awards.tsx
@@ -7,11 +7,14 @@ export default function Awards({ data }: propsInterface) {
 
   return (
     <div className='flex-column'>
+      <h1 className='md:hidden flex justify-center'>Awards</h1>
       <ul className=' overflow-y-auto w-full'>
         {lower_content[0].map((item, i) => (
-          <li key={i} className='flex flex-row justify-between mt-2'>
+          <li
+            key={i}
+            className='flex flex-row justify-between mt-2 items-center'>
             <p className='ml-5 my-2'>{item[0]}</p>
-            <p className='mr-5 italic'>{item[1]}</p>
+            <p className='mr-5 italic text-right'>{item[1]}</p>
           </li>
         ))}
       </ul>
