7bc41d26c9db1e957547b581a022e99311da464a
dham19 <david.ham0099@gmail.com>
2024-02-07 21:38:33 -0500

    mobile responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
-import FlippingCard from "./components/FlippingCard";
-import Navbar from "./components/Navbar";
-import WhiteDivider from "./components/WhiteDivider";
-import Contact from "./components/Contact";
-import About from "./components/About";
-import GetEssay from "./components/GetEssay";
+import FlippingCard from './components/FlippingCard';
+import Navbar from './components/Navbar';
+import WhiteDivider from './components/WhiteDivider';
+import Contact from './components/Contact';
+import About from './components/About';
+import GetEssay from './components/GetEssay';
+import MobileSections from './components/MobileSections';
 
 export default function Home() {
   return (
     <main>
       <Navbar />
-      <FlippingCard />
+      <div className='hidden md:block'>
+        <FlippingCard />
+      </div>
+      <div className='block md:hidden'>
+        <MobileSections />
+      </div>
       <WhiteDivider />
       <About>
         <GetEssay />
